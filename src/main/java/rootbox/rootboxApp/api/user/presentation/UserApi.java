e5935a234a84eb9adff95ca7acb92878716348af
CYY1007 <younguk86@gmail.com>
2025-07-06 14:49:13 +0900

    .

--- src/main/java/rootbox/rootboxApp/api/user/presentation/UserApi.java
+++ src/main/java/rootbox/rootboxApp/api/user/presentation/UserApi.java
@@ -18,24 +18,26 @@ import java.io.IOException;
 @Slf4j
 @Validated
 @Tag(name = "User Api", description = "rootbox 사용자 관련 Api입니다.")
-@RequestMapping(value = "/api/v1/users")
 public class UserApi {
 
     private final UserService userService;
 
-    @GetMapping(value = "/auth/health")
+    @GetMapping(value = "/api/v1/user/health")
+    public String health2() {return "I'm healthy!!!" ;}
+
+    @GetMapping(value = "/api/v1/users/auth/health")
     public String health() {return "I'm healthy!!!" ;}
 
-    @PostMapping(value = "/auth/kakao")
+    @PostMapping(value = "/api/v1/users/auth/kakao")
     public CommonResponse<SocialLoginDto.KakaoSocialLoginResponseDto> kakaoSocialLogin(@RequestBody @Valid SocialLoginDto.KakaoSocialLoginRequestDto requestDto) {
         return CommonResponse.onSuccess(userService.socialLogin(requestDto));
     }
-    @GetMapping(value = "/auth/kakao/code")
+    @GetMapping(value = "/api/v1/users/auth/kakao/code")
     public void kakaoSocailLoginTest(HttpServletResponse response) throws IOException {
         response.sendRedirect(userService.getKakaoCode());
     }
 
-    @GetMapping(value = "/auth/kakao/test")
+    @GetMapping(value = "/api/v1/users/auth/kakao/test")
     public CommonResponse<String> getKakaoToken(@RequestParam("code") String code){
         return CommonResponse.onSuccess(userService.getKakaoToken(code));
     }
--- src/main/java/rootbox/rootboxApp/global/security/config/SecurityConfig.java
+++ src/main/java/rootbox/rootboxApp/global/security/config/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
                         authorize -> {
 //                            authorize.requestMatchers("/swagger-ui/**").permitAll();
                             authorize.requestMatchers("/api/v1/users/auth/**").permitAll();
+                            authorize.requestMatchers("/api/v1/user/**").permitAll();
                             authorize.anyRequest().authenticated();
                         })
                 .exceptionHandling(
